20f5a113f5b08f840d7b47b3211f236803e76783
이정민 <jungmin@ijeongmin-ui-MacBookPro.local>
2023-10-29 21:21:02 +0900

    fix: Header UI 수정(인증 유저 추가)

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,16 +1,28 @@
 import { Link, useNavigate } from 'react-router-dom';
 import Button from './Button';
 import styles from './Header.module.scss';
+import { useAuthContext } from '../../context/AuthContext';
 
 export default function Header() {
+  const { user, login, logout } = useAuthContext();
   const navigate = useNavigate();
 
-  const handleLoginClick = () => {
-    navigate('/login');
+  const handleClick = () => {
+    if (user) {
+      logout();
+      navigate('/');
+      return;
+    }
+
+    login();
   };
 
   const handleSignupClick = () => {
     navigate('/signup');
+  };
+
+  const handleMyWonderBoxClick = () => {
+    navigate('/calendars/:calendarId/share');
   };
   return (
     <div className={styles.header}>
@@ -18,12 +30,19 @@ export default function Header() {
         WonderBox
       </Link>
       <div className={styles.buttons}>
-        <Button className={styles.button} onClick={handleLoginClick}>
-          로그인
+        <Button className={styles.button} onClick={handleClick}>
+          {user ? '로그아웃' : ' 로그인'}
         </Button>
-        <Button className={styles.button} onClick={handleSignupClick}>
-          회원가입
-        </Button>
+        {!user ? (
+          <Button className={styles.button} onClick={handleSignupClick}>
+            회원가입
+          </Button>
+        ) : (
+          <Button className={styles.button} onClick={handleMyWonderBoxClick}>
+            My Wonderbox
+          </Button>
+        )}
+        {}
       </div>
     </div>
   );
