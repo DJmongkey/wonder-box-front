c1cca1ed89cdaebe9e3e7fc494a32dbc095406cf
selene-cho <selene.cho29@gmail.com>
2023-10-30 05:53:13 +0900

    feat: 버튼 컴포넌트 생성

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,3 +1,38 @@
-export default function Button() {
-  return <div>Button</div>;
+import { Link } from 'react-router-dom';
+
+import styles from './Button.module.scss';
+
+export default function Button({
+  children,
+  to,
+  type = 'button',
+  onClick,
+  disabled,
+  customClass,
+}) {
+  const className = [
+    styles.button,
+    disabled ? styles['button--disabled'] : '',
+    customClass || '',
+  ].join(' ');
+
+  return (
+    <>
+      {to && (
+        <Link to={to} className={styles.button}>
+          {children}
+        </Link>
+      )}
+      {!to && (
+        <button
+          className={className}
+          type={type}
+          onClick={onClick}
+          disabled={disabled}
+        >
+          {children}
+        </button>
+      )}
+    </>
+  );
 }
