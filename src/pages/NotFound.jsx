747380367602a3e50edddd40dd590b0b5879c2b7
selene-cho <selene.cho29@gmail.com>
2023-11-01 04:11:35 +0900

    fix: NotFound 백엔드 에러메세지, 코드 받도록 수정

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { FcHome } from 'react-icons/fc';
 import { FiAlertTriangle } from 'react-icons/fi';
 
@@ -6,20 +6,23 @@ import Button from '../components/shared/Button';
 import styles from './NotFound.module.scss';
 
 export default function NotFound() {
-  const error = useRouteError();
-  console.error(error);
+  const location = useLocation();
+
+  const errorMessage = location.state?.errorMessage || 'Not Found';
+  const errorStatus = location.state?.errorStatus || '404';
 
   return (
     <div className={styles.container}>
       <div className={styles.description}>
         <h2>
-          <FiAlertTriangle className={styles.icon} /> 404
+          <FiAlertTriangle className={styles.icon} />
+          {errorStatus}
         </h2>
         <p>해당 페이지를 찾지 못했습니다</p>
         <p>주소가 잘못되었거나, 더 이상 제공하지않는 페이지입니다.</p>
       </div>
       <div className={styles.wrapper}>
-        <pre className={styles.error}>{error.statusText || error.message}</pre>
+        <pre className={styles.error}>{errorMessage}</pre>
         <Button to="/" customClass={styles.toMain}>
           <FcHome className={styles.home} />
           <p>메인페이지로</p>
