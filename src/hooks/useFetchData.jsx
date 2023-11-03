1e3a9a1281c0472be3f59a649e1d8ce384559dae
selene-cho <selene.cho29@gmail.com>
2023-11-04 04:14:31 +0900

    feat: data fetch 로직, form input 로직 커스텀 훅으로 분리

---
+++ src/hooks/useFetchData.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import ERRORS from '../errors/errorMessage';
+
+export default function useFetchData() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  async function fetchData(
+    url,
+    method = 'GET',
+    headers = {},
+    body = null,
+    accessToken,
+  ) {
+    setIsLoading(true);
+
+    if (!accessToken) {
+      navigate('/notfound', {
+        state: {
+          errorMessage: ERRORS.AUTH.NOT_FOUND_TOKEN,
+          errorStatus: 401,
+        },
+      });
+    }
+
+    try {
+      const res = await fetch(`${import.meta.env.VITE_BASE_URL}${url}`, {
+        method,
+        headers,
+        body,
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new HttpError(data.status, data.message);
+      }
+
+      setIsLoading(false);
+
+      return data;
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+
+      throw error;
+    }
+  }
+
+  return { fetchData, isLoading, error, navigate };
+}
