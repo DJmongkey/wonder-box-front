8811f221465dc3bd54a631664353823355cc9909
selene-cho <selene.cho29@gmail.com>
2023-11-04 04:12:04 +0900

    feat: input 컴포넌트 분리

---
+++ src/components/shared/Input.jsx
@@ -0,0 +1,70 @@
+import styles from './Input.module.scss';
+
+export default function Input({
+  type = 'text',
+  id,
+  name,
+  value,
+  onChange,
+  label,
+  isRequired = true,
+  className,
+  error,
+  subText,
+  checkedOption,
+}) {
+  function checkInputType() {
+    switch (type) {
+      case 'text':
+        return (
+          <div className={className}>
+            {label && <label htmlFor={id}>{label}</label>}
+            <input
+              type="text"
+              id={id}
+              name={name}
+              value={value}
+              onChange={onChange}
+              required={isRequired}
+            />
+            {error && <div className={styles.error}>{error}</div>}
+          </div>
+        );
+      case 'date':
+        return (
+          <div className={`${className} ${styles.dateInput}`}>
+            {label && <label htmlFor={id}>{label}</label>}
+            <input
+              type="date"
+              id={id}
+              name={name}
+              value={value}
+              onChange={onChange}
+              required={isRequired}
+            />
+            <p>{subText}</p>
+            {error && <div className={styles.error}>{error}</div>}
+          </div>
+        );
+      case 'radio':
+        return (
+          <div className={`${className} ${styles.radioInput}`}>
+            <input
+              type="radio"
+              id={id}
+              name={name}
+              value={value}
+              onChange={onChange}
+              required={isRequired}
+              checked={checkedOption}
+            />
+            {label && <label htmlFor={id}>{label}</label>}
+            {error && <div className={styles.error}>{error}</div>}
+          </div>
+        );
+      default:
+        return null;
+    }
+  }
+  return checkInputType();
+}
