0ad80b91a1f10bc89f8eb6ac0f9c0a3b776d1634
이정민 <jungmin@ijeongmin-ui-MacBookPro.local>
2023-11-11 01:14:44 +0900

    chore: 로그인 후 페이지 이동 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -39,7 +39,7 @@ export default function Login() {
       setIsLoading(false);
 
       login(data.accessToken);
-      navigate('/');
+      navigate('/custom/base-info');
     } catch (error) {
       setIsLoading(false);
       setError(
